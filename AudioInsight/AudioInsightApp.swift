535deb7eb2bbfbe76dc5de5ff3cb476d874f3e84
GradByte <eertekin99@outlook.com>
2026-06-09 13:14:14 +0300

    Initial Commit

---
+++ AudioInsight/AudioInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioInsightApp.swift
+//  AudioInsight
+//
+//  Created by Efe Ertekin on 9.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioInsight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioInsight
+//
+//  Created by Efe Ertekin on 9.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
